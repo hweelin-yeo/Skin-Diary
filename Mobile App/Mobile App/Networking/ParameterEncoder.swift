b8b1374e8cf06b1633842cd35840a553696cef8c
hweelin-yeo <hy384@cornell.edu>
2019-10-25 16:47:01 -0400

    add mobile app

---
+++ Mobile App/Mobile App/Networking/ParameterEncoder.swift
@@ -0,0 +1,51 @@
+//
+//  ParameterEncoder.swift
+//  Mobile App
+//
+//  Created by Yeo Hwee Lin on 10/25/19.
+//  Copyright © 2019 Yeo Hwee Lin. All rights reserved.
+//
+
+import Foundation
+
+public protocol ParameterEncoder {
+    static func encode(urlRequest: inout URLRequest, with parameters: Parameters) throws
+}
+
+public struct URLParameterEncoder: ParameterEncoder {
+    
+    public static func encode(urlRequest: inout URLRequest, with parameters: Parameters) throws {
+        
+        guard let url = urlRequest.url else { throw NetworkError.missingURL }
+        
+        if var urlComponents = URLComponents(url: url, resolvingAgainstBaseURL: false), !parameters.isEmpty {
+            
+            urlComponents.queryItems = [URLQueryItem]()
+            
+            for (key, value) in parameters {
+                let queryItem = URLQueryItem(name: key, value: "\(value)".addingPercentEncoding(withAllowedCharacters: .urlHostAllowed))
+                urlComponents.queryItems?.append(queryItem)
+            }
+            urlRequest.url = urlComponents.url
+        }
+        
+        if urlRequest.value(forHTTPHeaderField: "Content-Type") == nil {
+            urlRequest.setValue("application/x-www-form-urlencoded; charset=utf-8", forHTTPHeaderField: "Content-Type")
+        }
+    }
+}
+
+
+public struct JSONParameterEncoder: ParameterEncoder {
+    public static func encode(urlRequest: inout URLRequest, with parameters: Parameters) throws {
+        do {
+            let jsonAsData = try JSONSerialization.data(withJSONObject: parameters, options: .prettyPrinted)
+            urlRequest.httpBody = jsonAsData
+            if urlRequest.value(forHTTPHeaderField: "Content-Type") == nil {
+                urlRequest.setValue("application/json", forHTTPHeaderField: "Content-Type")
+            }
+        } catch {
+            throw NetworkError.encodingFailed
+        }
+    }
+}
---
+++ Mobile App/Mobile App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Mobile App
+//
+//  Created by Yeo Hwee Lin on 10/25/19.
+//  Copyright © 2019 Yeo Hwee Lin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
