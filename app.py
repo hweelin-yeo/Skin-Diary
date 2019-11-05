6ed1c1a5313a02e2313679c3dc7ae479acb66e45
Sachi Angle <sachiangle@gmail.com>
2019-11-05 15:13:44 -0500

    edited hello

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
-from flask import Flask
+from flask import Flask, request
+from userLog import create_user
+from init import db
 
 app = Flask(__name__)       # create an app instance
 
 @app.route("/")             # at end point /
 
 def hello():
-	return "User logged in!"
+    userId = request.args.get('userId')
+    timeStamp = request.args.get('timeStamp')
+    return create_user(db, userId, timeStamp)
+    
 
 if __name__ == "__main__":  # on running python app.py
 	app.run()
