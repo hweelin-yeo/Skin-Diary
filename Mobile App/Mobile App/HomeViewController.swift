197e6450a0f0f8a42df89d237953807b9f93337b
hweelin-yeo <hy384@cornell.edu>
2019-11-09 18:39:19 -0500

    init files

--- Mobile App/Mobile App/HomeViewController.swift
+++ Mobile App/Mobile App/HomeViewController.swift
@@ -8,11 +8,15 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    func setupGreetings() {
+        
     }
 
 
