4f95789ea44eb456bcba95d62beb70e421d09ac5
Sachi Angle <sachiangle@gmail.com>
2019-11-18 21:59:07 -0500

    table creation changes

--- DBMS/initialization/create.sql
+++ DBMS/initialization/create.sql
@@ -1,11 +1,12 @@
 USE skinDiary;
 
 CREATE TABLE customerPurchaseHistory(
-    `customerId` VARCHAR(200) PRIMARY KEY,
-    `purchaseOrder` INT UNSIGNED,
+    `purchaseOrder` INT UNSIGNED PRIMARY KEY,
+    `customerId` VARCHAR(200),
     `zipcode` VARCHAR(7),
     `location` VARCHAR(200),	
     `productId` INT UNSIGNED,
+    `productName` VARCHAR(200),
     `category` VARCHAR(200)
 );
 
@@ -20,32 +21,34 @@ CREATE TABLE customer(
 CREATE TABLE bestSellers(
     `productId` INT UNSIGNED PRIMARY KEY,
     `name` VARCHAR(200),
-    `skinType` VARCHAR(200),
     `description` VARCHAR(200),
     `function` VARCHAR(200),
     `keywords` VARCHAR(200),
-    `keyIngredients` VARCHAR(200),
-    `concerns` VARCHAR(200)
+    `keyIngredients` VARCHAR(200)
 );
 
 CREATE TABLE bestSellersConcerns(
-    `productId` INT UNSIGNED PRIMARY KEY,
-    `concernId` INT UNSIGNED
+    `productId` INT UNSIGNED NOT NULL,
+    `concernId` INT UNSIGNED NOT NULL,
+    CONSTRAINT productId_concernId PRIMARY KEY(`productId`, `concernId`)
 );
 
 CREATE TABLE bestSellerSkintypes(
-    `productId` INT UNSIGNED PRIMARY KEY,
-    `skinTypeID` INT UNSIGNED
+    `productId` INT UNSIGNED NOT NULL,
+    `skinTypeID` INT UNSIGNED NOT NULL,
+    CONSTRAINT productId_concernId PRIMARY KEY(`productId`, `skinTypeID`)
 );
 
 CREATE TABLE concerns(
-    `concernId` INT UNSIGNED PRIMARY KEY,
-    `concern` VARCHAR(200)
+    `concernId` INT UNSIGNED NOT NULL,
+    `concern` VARCHAR(200) NOT NULL,
+    CONSTRAINT productId_concernId PRIMARY KEY(`concernId`, `concern`)
 );
 
 CREATE TABLE skinTypes(
-    `skinTypeID` INT UNSIGNED PRIMARY KEY,
-    `skinType` VARCHAR(200)
+    `skinTypeID` INT UNSIGNED NOT NULL,
+    `skinType` VARCHAR(200) NOT NULL,
+    CONSTRAINT productId_concernId PRIMARY KEY(`skinTypeID`, `skinType`)
 );
 
 
